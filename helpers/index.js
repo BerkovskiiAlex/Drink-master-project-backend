3a37f201a07a8f6266fac19d8a9d9ecc90e58029
BerkovskiiAlex <berkovskii845@gmail.com>
2023-11-15 23:55:45 +0200

    add birthdate and isAdult

--- helpers/index.js
+++ helpers/index.js
@@ -1,4 +1,5 @@
 /** @format */
 
 export { default as HttpError } from "./HttpError.js";
+export { default as isUserAdult } from "./isUserAdult.js";
 // export { default as sendEmail } from "./sendEmail.js";
