c4948b187f71745873f6101a9bfcb05a9e66b4d0
BerkovskiiAlex <berkovskii845@gmail.com>
2023-11-17 04:23:22 +0200

    add /mainpage

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ import logger from "morgan";
 import cors from "cors";
 import "dotenv/config";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from './swagger.json' assert {type: 'json'};
+import swaggerDocument from "./swagger.json" assert { type: "json" };
 
 import authRouter from "./routes/api/auth-router.js";
 import filtersRouter from "./routes/api/filters-router.js";
-// import drinksRouter from "./routes/api/drinks-router.js";
+import drinksRouter from "./routes/api/drinks-router.js";
 
 const app = express();
 
@@ -20,13 +20,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use("/api/auth", authRouter);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-// app.use("/api/filters", filtersRouter);
-
 app.use("/api/filters", filtersRouter);
-// app.use("/api/drinks", drinksRouter);
+app.use("/api/drinks", drinksRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
