f01bff6756eb4253ced0a9fab08dd87f9c2fea39
BerkovskiiAlex <berkovskii845@gmail.com>
2023-11-19 18:30:55 +0200

    add category on mainpage

--- controllers/drinks-controller.js
+++ controllers/drinks-controller.js
@@ -12,14 +12,20 @@ import { HttpError } from "../helpers/index.js";
 import { ctrlWrapper } from "../decorators/index.js";
 
 const getMainPageDrinks = async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, category } = req.query;
+
+  if (!category) {
+    throw HttpError(404, `category required`);
+  }
+
   const skip = (page - 1) * limit;
 
   const { isAdult = false } = req.user;
 
   const drinksCondition = isAdult
-    ? {}
+    ? { category }
     : {
+        category: category,
         alcoholic: "Non alcoholic",
       };
 
