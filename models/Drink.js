fb3dbfb812049fbba3af2b8a96e6baa62cb2e4c2
BerkovskiiAlex <berkovskii845@gmail.com>
2023-11-19 07:55:16 +0200

    add /search, /favorite, /:id

--- models/Drink.js
+++ models/Drink.js
@@ -7,9 +7,33 @@ import { handleSaveError, preUpdate } from "./hooks.js";
 
 const drinkSchema = new Schema(
   {
+    _id: { type: Schema.Types.ObjectId },
     drink: { type: String },
-    glass: { type: String },
+    drinkAlternate: { type: String },
+    tags: { type: String },
+    video: { type: String },
+    category: { type: String },
+    IBA: { type: String },
     alcoholic: { type: String },
+    glass: { type: String },
+    description: { type: String },
+    instructions: { type: String },
+    instructionsES: { type: String },
+    instructionsDE: { type: String },
+    instructionsFR: { type: String },
+    instructionsIT: { type: String },
+    instructionsRU: { type: String },
+    instructionsPL: { type: String },
+    instructionsUK: { type: String },
+    drinkThumb: { type: String },
+    ingredients: [
+      {
+        title: { type: String },
+        measure: { type: String },
+        ingredientId: { type: Schema.Types.ObjectId },
+      },
+    ],
+    shortDescription: { type: String },
   },
   { versionKey: false, timestamps: true }
 );
