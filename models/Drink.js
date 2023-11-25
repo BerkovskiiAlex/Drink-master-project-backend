03a1b33d70876ada01a7ed5a2e12c0af96c48115
BerkovskiiAlex <berkovskii845@gmail.com>
2023-11-25 13:53:06 +0200

    update

--- models/Drink.js
+++ models/Drink.js
@@ -57,16 +57,16 @@ const alcoholicList = ["Alcoholic", "Non alcoholic"];
 
 const drinkSchema = new Schema(
   {
-    drink: { type: String },
+    drink: { type: String, required: true },
     drinkAlternate: { type: String, default: "" },
     tags: { type: String, default: "" },
     video: { type: String, default: "" },
-    category: { type: String, enum: categoryList },
+    category: { type: String, enum: categoryList, required: true },
     IBA: { type: String, default: "" },
-    alcoholic: { type: String, enum: alcoholicList },
-    glass: { type: String, enum: glassList },
-    description: { type: String },
-    instructions: { type: String },
+    alcoholic: { type: String, enum: alcoholicList, required: true },
+    glass: { type: String, enum: glassList, required: true },
+    description: { type: String, required: true },
+    instructions: { type: String, required: true },
     instructionsES: { type: String, default: "" },
     instructionsDE: { type: String, default: "" },
     instructionsFR: { type: String, default: "" },
@@ -74,19 +74,20 @@ const drinkSchema = new Schema(
     instructionsRU: { type: String, default: "" },
     instructionsPL: { type: String, default: "" },
     instructionsUK: { type: String, default: "" },
-    drinkThumb: { type: String },
+    drinkThumb: { type: String, required: true },
     ingredients: [
       {
-        title: { type: String },
+        title: { type: String, required: true },
         measure: { type: String, default: "" },
         ingredientId: {
           type: Schema.Types.ObjectId,
           ref: "ingredient",
+          required: true,
         },
       },
     ],
     shortDescription: { type: String, default: "" },
-    owner: { type: Schema.Types.ObjectId },
+    owner: { type: Schema.Types.ObjectId, required: true },
   },
   { versionKey: false, timestamps: true }
 );
@@ -100,40 +101,3 @@ drinkSchema.post("findOneAndUpdate", handleSaveError);
 const Drink = model("drink", drinkSchema);
 
 export default Drink;
-
-// const drinkSchema = new Schema(
-//   {
-//     drink: { type: String, required: true },
-//     drinkAlternate: { type: String, default: "" },
-//     tags: { type: String, default: "" },
-//     video: { type: String, default: "" },
-//     category: { type: String, enum: categoryList, required: true },
-//     IBA: { type: String, default: "" },
-//     alcoholic: { type: String, enum: alcoholicList, required: true },
-//     glass: { type: String, enum: glassList, required: true },
-//     description: { type: String, required: true },
-//     instructions: { type: String, required: true },
-//     instructionsES: { type: String, default: "" },
-//     instructionsDE: { type: String, default: "" },
-//     instructionsFR: { type: String, default: "" },
-//     instructionsIT: { type: String, default: "" },
-//     instructionsRU: { type: String, default: "" },
-//     instructionsPL: { type: String, default: "" },
-//     instructionsUK: { type: String, default: "" },
-//     drinkThumb: { type: String, required: true },
-//     ingredients: [
-//       {
-//         title: { type: String, required: true },
-//         measure: { type: String, default: "" },
-//         ingredientId: {
-//           type: Schema.Types.ObjectId,
-//           ref: "ingredient",
-//           required: true,
-//         },
-//       },
-//     ],
-//     shortDescription: { type: String, default: "" },
-//     owner: { type: Schema.Types.ObjectId, required: true },
-//   },
-//   { versionKey: false, timestamps: true }
-// );
